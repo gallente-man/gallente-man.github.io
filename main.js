11a204abbd15403685664d047b257183bbef1f8b
gallente-man <gallente-man@hiddenlabs.net>
2022-11-28 21:27:05 +0800

    webpage update

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ var siteNavigator, tableRow, tableCell;
 siteNavigator = document.createElement("table");
 tableRow = siteNavigator.insertRow(0);
 tableCell = tableRow.insertCell(0);
-tableCell.innerHTML = "<a>首页</a>";
+tableCell.innerHTML = "<a>首页<br />Main Page</a>";
 tableCell.setAttribute("class","cellItem");
 tableCell = tableRow.insertCell(1);
-tableCell.innerHTML = "<a>为何盖伦特的船那么强？</a>";
+tableCell.innerHTML = "<a>为何盖伦特的船那么强？<br />Why Gallente Ship is Strong?</a>";
 tableCell.setAttribute("class","cellItem");
 tableCell = tableRow.insertCell(2);
-tableCell.innerHTML = "<a>关于</a>";
+tableCell.innerHTML = "<a>关于我们<br />About We</a>";
 tableCell.setAttribute("class","cellItem");
 siteNavigator.setAttribute("class","siteMenu");
 
