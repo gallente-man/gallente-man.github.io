1a0b4053394d1ca28268a8132b35959ce2405aa1
gallente-man <gallente-man@hiddenlabs.net>
2022-11-30 10:17:26 +0800

    webpage update

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-var siteNavigator, tableRow, tableCell;
+/* 脚本语言，Daisy Liao同学负责的 */
+
+/* 导航栏的链接 */
+var Link1, Link2, Link3;
+
+Link1 = document.createElement("a");
+Link1.setAttribute("href","index.html");
+Link1.innerHTML = "首页<br />Main Page";
+Link2 = document.createElement("a");
+Link2.setAttribute("href","ship.html");
+Link2.innerHTML = "为何盖伦特的船那么强？<br />Why Gallente Ship is Strong?";
+Link3 = document.createElement("a");
+Link3.setAttribute("href","about.html");
+Link3.innerHTML = "关于我们<br />About We";
+
+/* 导航栏的表格 */
+var siteNavigator, tableRow, tableCell, tableLink;
 
 siteNavigator = document.createElement("table");
 tableRow = siteNavigator.insertRow(0);
 tableCell = tableRow.insertCell(0);
-tableCell.innerHTML = "<a>首页<br />Main Page</a>";
 tableCell.setAttribute("class","cellItem");
+tableLink = tableCell.appendChild(Link1);
 tableCell = tableRow.insertCell(1);
-tableCell.innerHTML = "<a>为何盖伦特的船那么强？<br />Why Gallente Ship is Strong?</a>";
 tableCell.setAttribute("class","cellItem");
+tableLink = tableCell.appendChild(Link2);
 tableCell = tableRow.insertCell(2);
-tableCell.innerHTML = "<a>关于我们<br />About We</a>";
 tableCell.setAttribute("class","cellItem");
+tableLink = tableCell.appendChild(Link3);
 siteNavigator.setAttribute("class","siteMenu");
 
 document.getElementById("siteNav").appendChild(siteNavigator);
